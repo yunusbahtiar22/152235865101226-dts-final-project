dc47ace98d228f6191cba83c7609502404dac45e
yunusbahtiar22 <aboomarmoshab22@gmail.com>
2022-08-04 21:26:26 +0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,40 @@
 import React from "react";
+import { FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="w-screen flex-none bg-[#141414] h-24 flex justify-center items-center">
-      <h3 className="text-white font-semibold">
+    <footer className="w-screen bg-[#141414] p-2">
+      <div className="w-2/6 text-white mx-auto my-2">
+        <p className="text-gray-500 text-center">Stay in Touch</p>
+        <div className="flex p-2 justify-center gap-4 my-2">
+          <a
+            href="https://twitter.com/yunusbahtiar22"
+            target="_blank"
+            rel="noreferrer"
+            className="text-2xl">
+            <FaTwitter />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/yunus-bahtiar-1628271a0/"
+            className="text-2xl"
+            target="_blank"
+            rel="noreferrer">
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/yunusbahtiar22"
+            target="_blank"
+            rel="noreferrer"
+            className="text-2xl">
+            <FaGithub />
+          </a>
+        </div>
+      </div>
+      <p className="text-gray-500 text-center text-sm">
         Made with 🔥 by Yunus Bahtiar
-      </h3>
-    </div>
+      </p>
+      <p className="text-gray-500 text-center text-sm">DTS Final Project</p>
+    </footer>
   );
 };
 
